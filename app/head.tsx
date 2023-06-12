85ceac7f585a70733e24c433829279b452c50f20
servicesrhino <it.services.rhino@gmail.com>
2023-06-12 23:36:35 +0300

    initialized project and home page created

---
+++ app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>GhatGPT</title>
+      <meta
+        content="width=device-width,
+    initial-scale=1"
+        name="viewport"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.tsx',
+    './app/page.tsx',
+    './app/layout.tsx',
+    './components/**/*.tsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
